b0c859619a676c62a169e8683c6d4624de9a2c3e
ventParus <svyatoslav.stolyarov.s.sss2000@mail.ru>
2024-11-21 07:47:14 +0300

    пробовал сделать светофор

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Circle from "./components/Circle.vue"
+
+function startLight(){
+  document.getElementById("firstL").style.display ="none";
+}
+</script>
+
+<template>
+  <div>
+    <div id="firstL">
+      <Circle style = "background-color: red"></Circle>
+    </div>
+    <div id="secondL">
+      <Circle style = "background-color: yellow"></Circle>
+    </div>
+        <div id="lastL">
+      <Circle style = "background-color: green"></Circle>
+    </div>
+    <button @click="startLight">Start</button>
+  </div>
+</template>
+
+<style scoped>
+  button{
+    margin: 10px 0 0 0;
+  }
+</style>
---
+++ src/components/Circle.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div id="block">
+    
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    #block{
+      width: 100px;
+      height: 100px;
+        display: block;
+        border: 2px solid black;
+        border-radius: 50%;
+    }
+</style>
